270a6cdbcb6da1d5bfc9975753e999d7585e0813
Alejandro <alejandrosauke28@gmail.com>
2020-02-10 15:51:36 +0100

    insert

--- Proyecto01/src/servicios/insertarPelicula.java
+++ Proyecto01/src/servicios/insertarPelicula.java
@@ -1,5 +1,20 @@
+/**
+ * Insertar película
+ * 
+ * Realiza la inserción de datos de una nueva película.
+ * 
+ * Fecha: 10/02/2020
+ * 
+ * @author Alejandro Martín Martín
+ * @version 1.0
+ *
+ */
+
 package servicios;
 
+
 public class insertarPelicula {
+	
+	
 
 }
