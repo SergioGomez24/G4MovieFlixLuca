eed2cc09a7682384e9e14c85a00db1db69d2bf3a
Leo <lflorezmatute@gmail.com>
2020-02-10 21:45:32 +0100

    Revert "Update Pelicula.java"
    This reverts commit d512c9a29e6f98353e7ed91da9b3212dc59f0dc1.

--- Proyecto01/src/modelo/Pelicula.java
+++ Proyecto01/src/modelo/Pelicula.java
@@ -59,20 +59,18 @@ public class Pelicula {
 		this.categoria_pelicula = categoria_pelicula;
 	}
 	
-	public Pelicula crearPelicula() {
-		Pelicula p = null;
+	public void crearPelicula() {
+		
 		try {
-			p = new Pelicula();
-			p.setNombre_pelicula(LeerTeclado.leerPalabra("Introduce el nombre de la pelicula"));
-			p.setAnio_pelicula(LeerTeclado.leerInt("Introduce el año de la pelicula")); 
+			this.nombre_pelicula = LeerTeclado.leerPalabra("Introduce el nombre de la pelicula");
+			this.anio_pelicula = LeerTeclado.leerInt("Introduce el año de la pelicula");
 			System.out.println("Introduce el codigo de la categoria");
 			Categoria.mostrarCategorias();
-			p.setCategoria_pelicula(Categoria.dimeCategoria(LeerTeclado.leerInt()));
-			
+			this.categoria_pelicula = Categoria.dimeCategoria(LeerTeclado.leerInt());
 		} catch (Exception e) {
             e.getStackTrace();
 		}
-		return p;
+		
 	}
 	
 	//Metodo tostring
