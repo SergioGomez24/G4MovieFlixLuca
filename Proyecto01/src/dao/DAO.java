f40a39ec3a6b5cc86aa12896eddccb37f516a62c
David <davidheras19@gmail.com>
2020-02-10 16:27:46 +0100

    Create DAO.java

---
+++ Proyecto01/src/dao/DAO.java
@@ -0,0 +1,108 @@
+package dao;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+import conexion.Conexion;
+/**
+ * Clase DAO padre
+ * 
+ * Contiene información de cada Pelicula * 
+ * Fecha: 10/02/2020
+ * 
+ * @author Leonardo Flórez Matute
+ * @version 1.0
+ *
+ */
+public abstract class DAO {
+	protected Connection conexion;
+	protected Statement sta;
+	protected ResultSet rs;
+	protected ResourceBundle rb;
+	protected PreparedStatement pta;
+	protected CallableStatement cta;
+	
+	public DAO() {
+		Conexion con = new Conexion();
+		con.crearConexion();
+		conexion = con.getConexion();
+		rb = ResourceBundle.getBundle("jdbc.sql");
+	}
+	
+	/**
+	 * Liberamos recursos.
+	 * El proceso sera en el orden inverso de creacion.
+	 * 
+	 * @throws SQLException
+	 */
+	public void liberarRecursos() throws SQLException {
+		if (rs != null) {
+			// CERRAMOS EL RESULTSET
+			rs.close();
+		}
+		if (pta != null) {
+			// CERRAMOS EL PREPARESTATEMENT
+			pta.close();
+		}
+		if (sta != null) {
+			// CERRAMOS EL STATEMENT
+			sta.close();
+		}
+		if (cta != null) {
+			// CERRAMOS EL CALLABLESTATEMENT
+			cta.close();
+		}
+		if (conexion != null) {
+			// CERRAMOS LA CONEXION
+			conexion.close();
+		}
+	}
+	
+	/**
+	 * Proceso de inicio de la transaccion.
+	 */
+	public void iniciar_Transaccion() throws SQLException {
+		if (conexion != null) {
+			conexion.setAutoCommit(false);
+		}
+	}
+	
+	/**
+	 * Proceso de confirmacion de transaccion.
+	 * 
+	 * @throws SQLException
+	 */
+	public void confirmar_Transaccion() throws SQLException {
+		if (conexion != null) {
+			conexion.commit();
+		}
+	}
+	
+	/**
+	 * Proceso de anulacion de transaccion.
+	 * 
+	 * @throws SQLException
+	 */
+	public void anular_Transaccion() throws SQLException {
+		if (conexion != null) {
+			conexion.rollback();
+		}
+	}
+	
+	/**
+	 * Proceso para recibir la conexion de otro dao y poder resolver las
+	 * transacciones.
+	 * 
+	 * @param conexion
+	 */
+	public void recibir_Conexion(Connection conexion) throws SQLException {
+		liberarRecursos();
+		this.conexion = conexion;
+	}
+}
+
