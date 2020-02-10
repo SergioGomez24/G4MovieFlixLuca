260d27242c55ecc4a80739ed9d43f9ddd28245eb
Sergio Gomez <edmundhome@hotmail.com>
2020-02-10 15:30:38 +0100

    Clase Menu

---
+++ Proyecto01/src/gui/Menu.java
@@ -0,0 +1,26 @@
+/**
+ * Clase Menu
+ * 
+ * Contiene el metodo para mostrar el menu
+ * 
+ * Fecha: 10/02/2020
+ * 
+ * @author Sergio Gómez Benítez
+ * @version 1.0
+ *
+ */
+
+package gui;
+
+public class Menu {
+	
+	public void mostrarMenu() {
+		
+		System.out.println("------ MENU ------");
+		System.out.println("Introduce alguna de estas opciones");
+		System.out.println("1. Alta Pelicula");
+		System.out.println("0. Salir");
+		
+	}
+
+}
