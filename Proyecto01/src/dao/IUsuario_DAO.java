72b634edb77eed4d5bb503871844849e4d682678
Alejandro <alejandrosauke28@gmail.com>
2020-02-11 12:16:42 +0100

    Update IUsuario_DAO.java

--- Proyecto01/src/dao/IUsuario_DAO.java
+++ Proyecto01/src/dao/IUsuario_DAO.java
@@ -1,11 +1,14 @@
 package dao;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+import modelo.Pelicula;
 import modelo.Usuario;
 
 public interface IUsuario_DAO {
 
 	boolean altaUsuario(Usuario usu) throws SQLException;
+	public ArrayList<Usuario> listarUsuario() throws SQLException;
 
 }
