2ce54d55618fb74d9dbd0e1c74c8c2b26ef95814
Alejandro <alejandrosauke28@gmail.com>
2020-02-11 14:48:13 +0100

    Update IGestion_Usuarios.java

--- Proyecto01/src/servicios/IGestion_Usuarios.java
+++ Proyecto01/src/servicios/IGestion_Usuarios.java
@@ -22,6 +22,7 @@ public interface IGestion_Usuarios {
 	public void altaUsuario(Usuario usu);
 	public ArrayList<Usuario> listarUsuario();
 	public void mostrarUsuario();
+	public void modificarUsuario();
 	public void modificarUsuario(Usuario usu);
 
 }
