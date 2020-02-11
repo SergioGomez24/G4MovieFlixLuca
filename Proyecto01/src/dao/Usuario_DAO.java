c583bc8acd4afff8df7ae5ac6143583fc9b79ee9
Leo <lflorezmatute@gmail.com>
2020-02-11 15:48:30 +0100

    Revert "Update Usuario_DAO.java"
    This reverts commit d3f1c873ef0b936cf532ce2cf1ba1385dfb78de4.

--- Proyecto01/src/dao/Usuario_DAO.java
+++ Proyecto01/src/dao/Usuario_DAO.java
@@ -2,8 +2,10 @@ package dao;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 import modelo.Usuario;
 
 public class Usuario_DAO extends DAO implements IUsuario_DAO {
@@ -20,9 +22,7 @@ public class Usuario_DAO extends DAO implements IUsuario_DAO {
 		super();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 *
+	/* (non-Javadoc)
 	 * @see dao.IUsuario_DAO#altaUsuario(modelo.Usuario)
 	 */
 	@Override
@@ -42,19 +42,21 @@ public class Usuario_DAO extends DAO implements IUsuario_DAO {
 			return false;// el insert ha fallado
 		}
 	}
-
+	
 	@Override
 	public ArrayList<Usuario> listarUsuario() throws SQLException {
 		ArrayList<Usuario> lista = new ArrayList<Usuario>();
 		String consulta = rb.getString("listar.usuarios");
 		pta = conexion.prepareStatement(consulta);
 		rs = pta.executeQuery();
-		while (rs.next()) {
+		
+		while(rs.next()) {
 			Usuario u = new Usuario();
 			u.setNombre_Usuario(rs.getString("nombre_Usuario"));
 			u.setFechaNac_Usuario(rs.getDate("fechaNac_Usuario"));
 			u.setResidencia_Usuario(rs.getString("residencia_Usuario"));
 			u.setNick_Usuario(rs.getString("nick_Usuario"));
+			
 			lista.add(u);
 		}
 		return lista;
@@ -62,7 +64,7 @@ public class Usuario_DAO extends DAO implements IUsuario_DAO {
 
 	@Override
 	public boolean modificarUsuario(Usuario usu) throws SQLException {
-		String update = rb.getString("modificar.usuarios");
+		String update = rb.getString("modificar.usuario");
 		pta = conexion.prepareStatement(update);
 		pta.setString(1, usu.getNombre_Usuario());
 		pta.setDate(2, usu.getFechaNac_Usuario());
@@ -76,19 +78,23 @@ public class Usuario_DAO extends DAO implements IUsuario_DAO {
 			logger.info("Modificación erronea");
 			return false;
 		}
+		
 	}
-
-	public boolean eliminarUsuario(String nick) throws SQLException {
-		String borrar = rb.getString("eliminar.usuario");
-		pta = conexion.prepareStatement(borrar);
+	
+	public Usuario buscarPorNick(String nick) throws SQLException {
+		String buscar = rb.getString("buscar.usuario.nick");
+		pta = conexion.prepareStatement(buscar);
 		pta.setString(1, nick);
-		int num = pta.executeUpdate();
-		if (num == 1) {
-			logger.info("Eliminado correctamente");
-			return true;
-		} else {
-			logger.info("Eliminación erronea");
-			return false;
+		rs = pta.executeQuery();
+		
+		Usuario u = null;
+		if(rs.next()) {
+			u = new Usuario();
+			u.setNombre_Usuario(rs.getString("nombre_Usuario"));
+			u.setFechaNac_Usuario(rs.getDate("fechaNac_Usuario"));
+			u.setResidencia_Usuario(rs.getString("residencia_Usuario"));
+			u.setNick_Usuario(rs.getString("nick_Usuario"));
 		}
+		return u;
 	}
 }
