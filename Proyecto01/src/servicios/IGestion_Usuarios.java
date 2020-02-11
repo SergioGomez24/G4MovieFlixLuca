f9858d01ecd83812c4e47e207b5ddd7b787a0aa8
Alejandro <alejandrosauke28@gmail.com>
2020-02-11 11:44:11 +0100

    Update IGestion_Usuarios.java

--- Proyecto01/src/servicios/IGestion_Usuarios.java
+++ Proyecto01/src/servicios/IGestion_Usuarios.java
@@ -1,3 +1,14 @@
+/**
+ * Interfaz Gestion_Usuario
+ * 
+ * Creacion de la interfaz relativa al Usuario
+ * Fecha: 11/02/2020
+ * 
+ * @author Alejandro Martín Martín
+ * @version 1.0
+ *
+ */
+
 package servicios;
 
 public interface IGestion_Usuarios {
