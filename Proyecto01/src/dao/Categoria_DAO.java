23ef28fb40b66e7454165c1467dfaffd609e186e
David <davidheras19@gmail.com>
2020-02-12 11:35:40 +0100

    Create Categoria_DAO.java

---
+++ Proyecto01/src/dao/Categoria_DAO.java
@@ -0,0 +1,26 @@
+package dao;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import modelo.Categoria;
+
+public class Categoria_DAO extends DAO {
+	public Categoria_DAO() {
+		super();
+	}
+
+	public ArrayList<Categoria> recuperarCategorias() throws SQLException {
+		ArrayList<Categoria> lista = new ArrayList<Categoria>();
+		String consulta = rb.getString("listar.categorias");
+		pta = conexion.prepareStatement(consulta);
+		rs = pta.executeQuery();
+		Categoria c;
+		while (rs.next()) {
+			c = new Categoria();
+			c.setCodCategoria(rs.getInt("cod_categoria"));
+			c.setNombreCategoria(rs.getString("nombre_categoria"));
+			lista.add(c);
+		}
+		return lista;
+	}
+}
