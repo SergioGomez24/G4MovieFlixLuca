5fc256d2f98827d05336ec0d5932a5a461667594
Leo <lflorezmatute@gmail.com>
2020-02-12 11:50:18 +0100

    Update Vista.java

--- Proyecto01/src/gui/Vista.java
+++ Proyecto01/src/gui/Vista.java
@@ -1,10 +1,16 @@
 package gui;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Vista {
 	public static <T> void imprimirColeccion(ArrayList<T> arr) {
 		for(T r : arr)
 			System.out.println(r.toString());
+	}
+	
+	public static <K, V> void imprimirColeccion(Map<K,V> mapa) {
+		for (Map.Entry<K, V> entrada : mapa.entrySet())
+			System.out.println(entrada.getKey() + ": " + entrada.getValue().toString());
 	}
 }
